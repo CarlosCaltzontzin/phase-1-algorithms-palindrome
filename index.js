b91816f05756df2d224abf937c6273db2c22c902
Carlos Caltzontzin <cacr71993@yahoo.com>
2023-08-26 23:55:05 -0500

    completed phase-1-algorithms-palindrome

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const length = word.length;
+
+  // Iterate through half of the word
+  for (let i = 0; i < length / 2; i++) {
+    if (word[i] !== word[length - 1 - i]) {
+      return false; // If characters don't match, it's not a palindrome
+    }
+  }
+
+  return true; // If all characters match, it's a palindrome
 }
+
+console.log(isPalindrome("abba")); 
+console.log(isPalindrome("racecar")); 
+console.log(isPalindrome("a")); 
+console.log(isPalindrome("robot")); 
+console.log(isPalindrome("ab"));
 
 /* 
-  Add your pseudocode here
-*/
+  add your pseudocode here
+ */
 
-/*
-  Add written explanation of your solution here
-*/
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  /*
+    Add written explanation of your solution here
+  */
+  
+  // You can run `node index.js` to view these console logs
+  if (require.main === module) {
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
+  
+    console.log("");
+  
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
+  }
+  
+  module.exports = isPalindrome;
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
